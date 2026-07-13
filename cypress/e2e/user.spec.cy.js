03624fca5718161010a955f2ed97c450f480cfe0
bigwave_adm <iandramorais.gaia@gmail.com>
2026-07-13 17:29:51 +0100

    Add user update test with firstName, middleName, lastName selectors

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -5,16 +5,27 @@ describe('Orange HRM Tests', () => {
     passwordField: '[name="password"]',
     loginButton: '.oxd-button',
     sectionTitleTopBar: ':nth-child(8) > .oxd-main-menu-item',
+    dashboardGrid: ".orangehrm-dashboard-grid",
     wrongCredentialAlert: '.oxd-alert',
+    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameField: "[name='firstName']",
+    middleName: "[name='middleName']",
+    lastNameField: "[name='lastName']"
+
   }
    
-  it('Login - Success', () => {
+  it.only('User Infor Update - Success', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-  cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
+    cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click ()
+    cy.url().should('include', '/pim/viewPersonalDetails')
+    cy.get(selectorsList.firstNameField).clear().type('Novo Nome')
+    cy.get(selectorsList.middleName).clear().type('Novo MiddleName')
+    cy.get(selectorsList.lastNameField).clear().type('Novo Apelido')
   })
 it('Login - Fail', () => {
     cy.visit('/auth/login')
